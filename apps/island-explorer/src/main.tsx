9251ddfe4f10c312d70f820790a5a87e820698c1
Rich McNeary <rlmcneary2@gmail.com>
2020-05-06 13:44:39 -0400

    Create workspace.

---
+++ apps/island-explorer/src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <p>Hello world!</p>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
