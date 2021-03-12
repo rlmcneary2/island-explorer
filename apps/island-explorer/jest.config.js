3beca0859dfa7853497435494853243a75792cb9
Rich McNeary <rlmcneary2@users.noreply.github.com>
2021-03-12 03:54:42 +0000

    v10

--- apps/island-explorer/jest.config.js
+++ apps/island-explorer/jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  name: 'island-explorer',
-  preset: '../../jest.config.js',
+  preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': 'ts-jest'
+    '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/apps/island-explorer'
+  coverageDirectory: '../../coverage/apps/island-explorer',
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
+  displayName: 'island-explorer',
 };
