a157f9e8e489cdecfbfd078bb225055784b9a735
Rich McNeary <rlmcneary2@users.noreply.github.com>
2021-03-12 04:08:39 +0000

    v11

--- apps/island-explorer/src/app.tsx
+++ apps/island-explorer/src/app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { css, Global } from '@emotion/core';
+import { css, Global } from '@emotion/react';
 import Main from './components/main';
 import { ContentStateProvider } from './state/content/content-context';
 
