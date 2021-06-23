c274dde4b6365d25c4b9fd7e276964aacfd71bca
rlmcneary2 <rlmcneary2@gmail.com>
2021-06-22 23:12:06 -0400

    Add service worker.

--- apps/island-explorer/src/app.tsx
+++ apps/island-explorer/src/app.tsx
@@ -6,6 +6,15 @@ import Main from "./components/main";
 import { ContextProvider } from "./context/context-provider";
 import messages from "./assets/messages-en-us.json";
 
+navigator.serviceWorker
+  .register("service-worker.js")
+  .then(args => {
+    console.log("Service worker success. ", args);
+  })
+  .catch(err => {
+    console.log("Service worker failure. ", err);
+  });
+
 ReactDOM.render(
   <BrowserRouter>
     <IntlProvider locale="en-US" messages={messages}>
--- libs/service-worker/src/service-worker.ts
+++ libs/service-worker/src/service-worker.ts
@@ -1,1 +1,7 @@
-console.log("service worker");
+self.addEventListener("install", function () {
+  console.log("service worker install event");
+});
+
+self.addEventListener("activate", function () {
+  console.log("service worker activate event");
+});
