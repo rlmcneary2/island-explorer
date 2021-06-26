8d791879f0c7c974a29e31c1960c63597381ceff
rlmcneary2 <rlmcneary2@gmail.com>
2021-06-26 15:21:31 -0400

    Initial cache for routes.

--- libs/service-worker/src/service-worker.ts
+++ libs/service-worker/src/service-worker.ts
@@ -1,7 +1,37 @@
-self.addEventListener("install", function () {
+const version = "1";
+
+const wgs = (self as unknown) as ServiceWorkerGlobalScope;
+
+wgs.addEventListener("install", event => {
   console.log("service worker install event");
+
+  return event.waitUntil(
+    caches
+      .open(version)
+      .then((cache: Cache) =>
+        cache.addAll([
+          "https://acadia-explorer.netlify.app/api/InfoPoint/rest/Routes/GetVisibleRoutes",
+        ])
+      )
+  );
 });
 
-self.addEventListener("activate", function () {
+wgs.addEventListener("activate", () => {
   console.log("service worker activate event");
+});
+
+wgs.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then((response: Response) => {
+      // TODO: handle expiring the cache.
+
+      if (response) {
+        console.log("service worker cache entry found for ", event.request);
+        return response;
+      }
+
+      console.log("service worker NO cache entry for ", event.request);
+      return fetch(event.request);
+    })
+  );
 });
