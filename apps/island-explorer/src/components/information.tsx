04c2b860aa81f071a02a7d6d6c10d01dde287d3f
rlmcneary2 <rich@bitovi.com>
2021-07-30 23:32:33 -0400

    Landmark and route json data for infomration.

--- apps/island-explorer/src/components/information.tsx
+++ apps/island-explorer/src/components/information.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import { routes } from "../assets/routes.json";
+import { landmarks } from "../assets/landmarks.json";
 
 export default function Information({ routeId }: Props) {
+  const route = routes.find(r => r.id === routeId);
+  if (!route) {
+    return null;
+  }
+
+  const { scheduledStops, ...displayRoute } = route;
+
+  const routeLandmarks = landmarks.filter(l =>
+    route.landmarks.some(rl => rl === l.id)
+  );
+
   return (
-    <div className="information">{`Information route ${routeId || "?"}`}</div>
+    <div className="information">
+      <span>{`Information route ${routeId || "?"}`}</span>
+      <pre>{JSON.stringify(displayRoute, null, 2)}</pre>
+      <pre>{JSON.stringify(routeLandmarks, null, 2)}</pre>
+    </div>
   );
 }
 
