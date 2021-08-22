aee90d68323593c03f9590f119e924f5194d35bb
rlmcneary2 <rlmcneary2@gmail.com>
2021-08-21 22:50:03 -0400

    Make RoutesModal animated.

--- apps/island-explorer/src/components/routes-modal.tsx
+++ apps/island-explorer/src/components/routes-modal.tsx
@@ -1,21 +1,42 @@
+import { useCallback, useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import { ModalDialog } from "modal";
+import { AnimatedModalDialog } from "modal";
 import type { ModalDialogProps } from "modal";
 import { ContextData } from "../context/types";
 import { getRouteParameters } from "../util/route";
 
 export function RoutesModal({ onClose, onExternalTap, routes }: Props) {
+  const [hide, setHide] = useState(false);
+  const callbackFunc = useRef<() => void>(null);
+
   const { page } = getRouteParameters();
-  const { data, error, status } = routes;
+  const { data, status } = routes;
+
+  const handleLinkClick = useCallback<Props["onClose"]>(() => {
+    callbackFunc.current = onClose;
+    setHide(true);
+  }, [onClose]);
+
+  const handleExternalTap = useCallback(() => {
+    callbackFunc.current = onExternalTap;
+    setHide(true);
+  }, [onExternalTap]);
+
+  const handleHidden = useCallback(
+    () => callbackFunc.current && callbackFunc.current(),
+    []
+  );
 
   if (status !== "idle") {
     return <p>loading...</p>;
   }
 
+  console.log("RoutesModal");
+
   const items = data.map(route => {
     return (
       <li key={route.RouteId}>
-        <Link onClick={onClose} to={`/${route.RouteId}/${page}`}>
+        <Link onClick={handleLinkClick} to={`/${route.RouteId}/${page}`}>
           {route.LongName}
         </Link>
       </li>
@@ -23,11 +44,16 @@ export function RoutesModal({ onClose, onExternalTap, routes }: Props) {
   });
 
   return (
-    <ModalDialog className="modal" onExternalTap={onExternalTap}>
+    <AnimatedModalDialog
+      className="modal"
+      hide={hide}
+      onExternalTap={handleExternalTap}
+      onHidden={handleHidden}
+    >
       <nav className="content">
         <ul>{items}</ul>
       </nav>
-    </ModalDialog>
+    </AnimatedModalDialog>
   );
 }
 
