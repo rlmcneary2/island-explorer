151a89e5a9b894bd3aa089465984463e6cff13ff
rlmcneary2 <rlmcneary2@gmail.com>
2022-05-29 02:39:51 -0400

    Cache the map and information paths.

--- libs/service-worker/src/service-worker.ts
+++ libs/service-worker/src/service-worker.ts
@@ -10,61 +10,68 @@ wgs.addEventListener("install", event => {
   const paths = [
     ...pathData.paths,
     ...pathData.apiLeftPaths.map(path => `${pathData.apiLeft}${path}`),
-    ...pathData.routeIds.map(
-      id => `${pathData.apiLeft}${pathData.stopsPath}${id}`
-    )
+    ...pathData.routeIds.reduce<string[]>((acc, id) => {
+      acc.push(
+        `${pathData.apiLeft}${pathData.stopsPath}${id}`,
+        `route/${id}/map`,
+        `route/${id}/information`
+      );
+      return acc;
+    }, [])
   ];
 
   event.waitUntil(
     caches
       .open(VERSION)
-      .then(cache => cache.addAll([...paths]))
+      .then(cache => cache.addAll(paths))
       .catch(err => console.error("service-worker: addAll err", err))
   );
 });
 
 wgs.addEventListener("fetch", async event => {
-  const response = await caches.match(event.request);
+  event.respondWith(
+    (async () => {
+      let response = await caches.match(event.request, { cacheName: VERSION });
 
-  if (!response) {
-    console.log(`service-worker: fetching '${event.request.url}'`);
-    // event.request.headers.forEach((...args) =>
-    //   console.log("service-worker: request headers=", args)
-    // );
-    const response = await fetch(event.request, { credentials: "same-origin" });
-    // try {
-    //   response.headers.forEach((...args) =>
-    //     console.log("service-worker: response headers=", args)
-    //   );
-    // } catch (err) {
-    //   console.log("service-worker: header error=", err);
-    // }
+      if (!response) {
+        console.log(`service-worker: fetching '${event.request.url}'`);
+        try {
+          response = await fetch(event.request, {
+            credentials: "same-origin"
+          });
+        } catch (err) {
+          console.log(
+            `service-worker: fetch error;\nurl='${event.request.url}'\nerr=`,
+            err
+          );
+        }
 
-    // Cache google fonts that vary by browser, etc.
-    const { pathname: requestPathname, hostname: requestHostname } = new URL(
-      event.request.url
-    );
-    if (
-      requestHostname === "fonts.googleapis.com" ||
-      requestHostname === "fonts.gstatic.com"
-    ) {
-      (await caches.open(VERSION)).put(event.request, response);
-      console.log(`service-worker: caching '${event.request.url}'`);
-    }
+        // Cache google fonts that vary by browser, etc.
+        const { pathname: requestPathname, hostname: requestHostname } =
+          new URL(event.request.url);
+        if (
+          requestHostname === "fonts.googleapis.com" ||
+          requestHostname === "fonts.gstatic.com"
+        ) {
+          console.log(`service-worker: caching '${event.request.url}'`);
+          (await caches.open(VERSION)).put(event.request, response.clone());
+        }
 
-    // Cache mapbox files
-    const mapboxUrl = `${requestHostname}${requestPathname}`;
-    if (
-      mapboxUrl.startsWith("api.mapbox.com/styles") ||
-      mapboxUrl.startsWith("api.mapbox.com/fonts")
-    ) {
-      (await caches.open(VERSION)).put(event.request, response);
-      console.log(`service-worker: caching '${event.request.url}'`);
-    }
+        // Cache mapbox files
+        const mapboxUrl = `${requestHostname}${requestPathname}`;
+        if (
+          mapboxUrl.startsWith("api.mapbox.com/styles") ||
+          mapboxUrl.startsWith("api.mapbox.com/fonts")
+        ) {
+          console.log(`service-worker: caching '${event.request.url}'`);
+          (await caches.open(VERSION)).put(event.request, response.clone());
+        }
 
-    return response;
-  } else {
-    console.log(`service-worker: CACHED '${event.request.url}'`);
-    return response;
-  }
+        return response;
+      } else {
+        console.log(`service-worker: CACHED '${event.request.url}'`);
+        return response;
+      }
+    })()
+  );
 });
