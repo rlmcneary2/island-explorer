d8d6536cb0fa9087bb33d2cb8b39322d88717dd9
rlmcneary2 <rlmcneary2@gmail.com>
2022-05-30 11:38:07 -0400

    Only set devServer config if it exists.

--- apps/island-explorer/webpack.config.js
+++ apps/island-explorer/webpack.config.js
@@ -1,9 +1,11 @@
-module.exports = config => {
+module.exports = (config, context) => {
   console.log(`---\n--- apps/island-explorer/webpack.config.js\n---`);
 
   // The cache.addAll requests don't include an Accept header that the dev
   // server likes so allow any mime type.
-  config.devServer.historyApiFallback.htmlAcceptHeaders = ["*/*"];
+  if (config.devServer) {
+    config.devServer.historyApiFallback.htmlAcceptHeaders = ["*/*"];
+  }
 
   return config;
 };
