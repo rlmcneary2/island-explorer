408487c8b3ef0b86221cd5ae9d7b47973a76ba6e
rlmcneary2 <rlmcneary2@gmail.com>
2022-06-19 17:37:38 -0400

    Update environment for new domain.

--- apps/island-explorer/src/environments/environment.ts
+++ apps/island-explorer/src/environments/environment.ts
@@ -1,6 +1,6 @@
 // apiLeft: "http://islandexplorertracker.availtec.com",
 
-const API_LEFT = "https://island-explorer.netlify.app/api";
+const API_LEFT = "https://islandexplorer.netlify.app/api";
 
 export const environment = {
   apiLeft: API_LEFT,
