d71f33114d711e4cdf0e70887e38851c1c195b1a
rlmcneary2 <rlmcneary2@gmail.com>
2022-07-04 00:18:32 -0400

    Fix alpha channel on icons.

--- libs/service-worker/src/service-worker.ts
+++ libs/service-worker/src/service-worker.ts
@@ -1,7 +1,7 @@
 import * as version from "./version.json";
 import * as pathData from "./paths.json";
 
-const CACHE_VERSION = "Island-Explorer-2021f";
+const CACHE_VERSION = "Island-Explorer-2021g";
 
 const wgs = self as unknown as ServiceWorkerGlobalScope;
 
