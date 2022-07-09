b80db7e1610e9e69e286819d5099de8a347853b8
rlmcneary2 <rlmcneary2@gmail.com>
2022-07-09 10:45:26 -0400

    Loop road and sand beach landmarks.

--- libs/service-worker/src/service-worker.ts
+++ libs/service-worker/src/service-worker.ts
@@ -1,7 +1,7 @@
 import * as version from "./version.json";
 import * as pathData from "./paths.json";
 
-const CACHE_VERSION = "Island-Explorer-2022l";
+const CACHE_VERSION = "Island-Explorer-2022m";
 
 const wgs = self as unknown as ServiceWorkerGlobalScope;
 
