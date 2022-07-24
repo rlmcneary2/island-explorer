f0b77c93e5c315b4430067e38958129d856f7d4f
rlmcneary2 <rlmcneary2@gmail.com>
2022-07-24 17:19:06 -0400

    Bump version to 1.0.9.

--- libs/service-worker/src/service-worker.ts
+++ libs/service-worker/src/service-worker.ts
@@ -1,7 +1,7 @@
 import * as version from "./version.json";
 import * as pathData from "./paths.json";
 
-const CACHE_VERSION = "Island-Explorer-2022n";
+const CACHE_VERSION = "Island-Explorer-2022o";
 
 const wgs = self as unknown as ServiceWorkerGlobalScope;
 
