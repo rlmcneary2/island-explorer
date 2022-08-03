64da247269a656f3ee178070162a9bf1c33d7191
rlmcneary2 <rich@bitovi.com>
2022-08-02 23:33:30 -0400

    Fix devServer configuration.

--- apps/island-explorer/webpack.config.js
+++ apps/island-explorer/webpack.config.js
@@ -3,13 +3,10 @@ const { merge } = require("webpack-merge");
 module.exports = (config, context) => {
   console.log(`---\n--- apps/island-explorer/webpack.config.js\n---`);
 
-  // The cache.addAll requests don't include an Accept header that the dev
-  // server likes so allow any mime type.
-  if (config.devServer) {
-    config.devServer.historyApiFallback.htmlAcceptHeaders = ["*/*"];
-  }
-
   return merge(config, {
+    devServer: {
+      historyApiFallback: true
+    },
     module: {
       rules: [
         {
