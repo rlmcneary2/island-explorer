f9e0d7cc7e4590afad36927c5e98acd042191df7
rlmcneary2 <rlmcneary2@gmail.com>
2023-06-14 16:16:32 -0400

    More route updates.

--- libs/service-worker/src/service-worker.ts
+++ libs/service-worker/src/service-worker.ts
@@ -1,7 +1,7 @@
 import * as version from "./version.json";
 import * as pathData from "./paths.json";
 
-const CACHE_VERSION = "Island-Explorer-2023b";
+const CACHE_VERSION = "Island-Explorer-2023c";
 
 const wgs = self as unknown as ServiceWorkerGlobalScope;
 
