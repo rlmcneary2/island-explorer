ab621c63f6cec99db9565ea43c98384a23086daa
rlmcneary2 <rlmcneary2@gmail.com>
2023-06-15 05:35:15 -0400

    Set link canonical tag in head.

--- apps/island-explorer/src/components/bus-route/bus-route.tsx
+++ apps/island-explorer/src/components/bus-route/bus-route.tsx
@@ -26,9 +26,36 @@ export function BusRoute() {
     setRoute(+paramRouteId);
   }, [paramRouteId, routeId, setRoute]);
 
+  setCanonical();
+
   return <BusRouteView />;
 }
 
 function selector({ routeId }: ContextData) {
   return { routeId };
+}
+
+/**
+ * Google doesn't believe that the data presented by /map and /information are
+ * different so it is not indexing each of those for a route. One way to change
+ * that (possibly) is to set a link in the header with `rel` = "canonical". This
+ * function updates a link tag based on the current location.
+ *
+ * See: https://developers.google.com/search/docs/crawling-indexing/javascript/javascript-seo-basics?hl=en#properly-inject-canonical-links
+ */
+function setCanonical() {
+  const link = document.head.querySelector("#canon") as HTMLLinkElement;
+  if (link) {
+    if (link.href === window.location.href) {
+      return;
+    }
+
+    link.remove();
+  }
+
+  const nextLink = document.createElement("link");
+  nextLink.id = "canon";
+  nextLink.href = window.location.href;
+  nextLink.setAttribute("rel", "canonical");
+  document.head.appendChild(nextLink);
 }
