8496fd707e7f53f6fbc9f72b589cba4f355296b3
rlmcneary2 <rlmcneary2@gmail.com>
2023-06-29 02:28:35 -0400

    Add Somesville bridge.

--- libs/service-worker/src/service-worker.ts
+++ libs/service-worker/src/service-worker.ts
@@ -1,7 +1,7 @@
 import * as version from "./version.json";
 import * as pathData from "./paths.json";
 
-const CACHE_VERSION = "Island-Explorer-2023k";
+const CACHE_VERSION = "Island-Explorer-2023l";
 
 const wgs = self as unknown as ServiceWorkerGlobalScope;
 
