0b5f15ce400ee648d2ccf692c5ca07faece17744
rlmcneary2 <rich@bitovi.com>
2023-07-13 08:10:12 -0400

    Fix bar harbor overlook routes.

--- libs/service-worker/src/service-worker.ts
+++ libs/service-worker/src/service-worker.ts
@@ -1,7 +1,7 @@
 import * as version from "./version.json";
 import * as pathData from "./paths.json";
 
-const CACHE_VERSION = "Island-Explorer-2023m";
+const CACHE_VERSION = "Island-Explorer-2023n";
 
 const wgs = self as unknown as ServiceWorkerGlobalScope;
 
