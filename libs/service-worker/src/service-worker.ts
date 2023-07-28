7b922452f61f9f635627528554454ac6a7381475
rlmcneary2 <rich@bitovi.com>
2023-07-28 17:55:48 -0400

    Start updates for sw cache paths.

--- libs/service-worker/src/service-worker.ts
+++ libs/service-worker/src/service-worker.ts
@@ -1,7 +1,7 @@
 import * as version from "./version.json";
 import * as pathData from "./paths.json";
 
-const CACHE_VERSION = "Island-Explorer-2023n";
+const CACHE_VERSION = "Island-Explorer-2023o";
 
 const wgs = self as unknown as ServiceWorkerGlobalScope;
 
