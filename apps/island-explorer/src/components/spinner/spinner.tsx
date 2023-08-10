c09dcd6faf532f406fe9ed98a46b241cc8f85a5c
rlmcneary2 <rlmcneary2@gmail.com>
2023-08-10 07:11:46 -0400

    Update spinner.

---
+++ apps/island-explorer/src/components/spinner/spinner.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { FormattedMessage } from "react-intl";
+import Icon from "../../assets/icon-bus.svg";
+
+export function Spinner({ message }: SpinnerProps) {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const id = setTimeout(() => setShow(true), 1000);
+    return () => clearTimeout(id);
+  }, []);
+
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className="spinner">
+      <div className="loading">
+        <Icon />
+      </div>
+      {message ? (
+        <span className="loading-message">
+          <FormattedMessage id={message} />
+        </span>
+      ) : null}
+    </div>
+  );
+}
+
+interface SpinnerProps {
+  message?: string;
+}
