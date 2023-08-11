82f580ad1fd6f5b2df15e2bd49e307a8ff18fda6
rlmcneary2 <rlmcneary2@gmail.com>
2023-08-10 22:55:25 -0400

    Load fonts from assets.

--- libs/service-worker/src/service-worker.ts
+++ libs/service-worker/src/service-worker.ts
@@ -1,7 +1,7 @@
 import * as version from "./version.json";
 import * as pathData from "./paths.json";
 
-const CACHE_VERSION = "Island-Explorer-2023p";
+const CACHE_VERSION = "Island-Explorer-2023q";
 
 const wgs = self as unknown as ServiceWorkerGlobalScope;
 
