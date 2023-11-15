475c8ed98e8ad1b1e2649936c1399058f421594b
rlmcneary2 <rlmcneary2@gmail.com>
2023-11-14 21:38:57 -0500

    Changes for React 18.

--- apps/island-explorer/src/app.tsx
+++ apps/island-explorer/src/app.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { IntlProvider } from "react-intl";
 import { ModalProvider } from "modal";
@@ -7,17 +7,18 @@ import { ContextProvider } from "./context/context-provider";
 import { ServiceWorkerProvider } from "service-worker-provider";
 import messages from "./assets/messages-en-us.json";
 
-ReactDOM.render(
-  <ServiceWorkerProvider url="service-worker.js" reloadOnSkipWaiting={true}>
-    <BrowserRouter>
-      <IntlProvider locale="en-US" messages={messages}>
-        <ModalProvider>
-          <ContextProvider>
-            <Main />
-          </ContextProvider>
-        </ModalProvider>
-      </IntlProvider>
-    </BrowserRouter>
-  </ServiceWorkerProvider>,
-  document.getElementById("root")
-);
+const root = document.getElementById("root");
+root &&
+  createRoot(root).render(
+    <ServiceWorkerProvider url="service-worker.js" reloadOnSkipWaiting={true}>
+      <BrowserRouter>
+        <IntlProvider locale="en-US" messages={messages}>
+          <ModalProvider>
+            <ContextProvider>
+              <Main />
+            </ContextProvider>
+          </ModalProvider>
+        </IntlProvider>
+      </BrowserRouter>
+    </ServiceWorkerProvider>
+  );
