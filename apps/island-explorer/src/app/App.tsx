57d98b93049ad975241de8c3ed612ce017daf91d
rlmcneary2 <rlmcneary2@gmail.com>
2024-04-24 23:12:34 -0400

    Setup routing.

---
+++ apps/island-explorer/src/app/App.tsx
@@ -0,0 +1,18 @@
+import { ServiceWorkerProvider } from "service-worker-provider";
+import { Layout } from "./components/Layout/Layout";
+import { IntlProvider } from "react-intl";
+import { ModalProvider } from "modal";
+import messages from "../assets/messages-en-us.json";
+
+export function App() {
+  // Providers go here
+  return (
+    <ServiceWorkerProvider url="service-worker.js" reloadOnSkipWaiting={true}>
+      <IntlProvider locale="en-US" messages={messages}>
+        <ModalProvider>
+          <Layout />
+        </ModalProvider>
+      </IntlProvider>
+    </ServiceWorkerProvider>
+  );
+}
--- apps/island-explorer/src/main.tsx
+++ apps/island-explorer/src/main.tsx
@@ -1,13 +1,32 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
+import { StrictMode } from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from "react-router-dom";
+import { App } from "./app/App";
 
-import App from './app/app';
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<App />}>
+      <Route element={<div>Map</div>} path="/route/:routeId/map" />
+      <Route
+        element={<div>Information</div>}
+        path="/route/:routeId/Information"
+      />
+      <Route element={<div>Directions</div>} path="/directions" />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
