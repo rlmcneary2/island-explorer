b826e8064ac0cfe4cfdf617454f695022a7ecbfd
rlmcneary2 <rlmcneary2@gmail.com>
2024-06-08 21:48:36 -0400

    Rename the Tremont route (back).

--- libs/service-worker/src/service-worker.ts
+++ libs/service-worker/src/service-worker.ts
@@ -1,7 +1,7 @@
 import * as version from "./version.json";
 import * as pathData from "./paths.json";
 
-const CACHE_VERSION = "Island-Explorer-2024b";
+const CACHE_VERSION = "Island-Explorer-2024c";
 
 const wgs = self as unknown as ServiceWorkerGlobalScope;
 
