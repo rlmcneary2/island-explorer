9d7e495cc973bcd0d5364b7b0ec35338277f6df9
rlmcneary2 <rlmcneary2@gmail.com>
2024-06-25 18:22:17 -0400

    Need to bump the service worker version to update the cache.

--- libs/service-worker/src/service-worker.ts
+++ libs/service-worker/src/service-worker.ts
@@ -1,7 +1,7 @@
 import * as version from "./version.json";
 import * as pathData from "./paths.json";
 
-const CACHE_VERSION = "Island-Explorer-2024c";
+const CACHE_VERSION = "Island-Explorer-2024d";
 
 const wgs = self as unknown as ServiceWorkerGlobalScope;
 
