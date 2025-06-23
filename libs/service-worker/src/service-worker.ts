64428aacfc3d35e4d0e3dc3defd8cdc99f54b587
Rich McNeary <rlmcneary2@gmail.com>
2025-06-23 19:27:06 -0400

    refactor: update bus route IDs

--- libs/service-worker/src/service-worker.ts
+++ libs/service-worker/src/service-worker.ts
@@ -1,7 +1,7 @@
 import * as version from "./version.json";
 import * as pathData from "./paths.json";
 
-const CACHE_VERSION = "Island-Explorer-2025a";
+const CACHE_VERSION = "Island-Explorer-2025b";
 
 const wgs = self as unknown as ServiceWorkerGlobalScope;
 
